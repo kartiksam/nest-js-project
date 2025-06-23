16f79420a572a78e9b0f61f72a5b226189b18a6a
kartik sharma <kartiksam123489@gmail.com>
2025-06-23 18:19:06 +0530

    swagger added

--- my-nest-app/src/main.ts
+++ my-nest-app/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Your-api-title")
+    .setDescription("Api-Description")
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
--- my-nest-app/src/modules/users/dto/create-user.dto.ts
+++ my-nest-app/src/modules/users/dto/create-user.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsOptional, IsNumber } from 'class-validator';
 
 export class CreateUserDto {
 
+    @ApiProperty()
     @IsString()
     name: string;
 
+    @ApiProperty()
     @IsEmail()
     email: string;
 
+    @ApiProperty()
     @IsOptional()
     @IsNumber()
     age: number;
